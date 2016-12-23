7ff4994d6be2b0064f1e5124981364658391c33f
Kawamura Shintaro <s.kawamura@zanon.co.jp>
2016-12-24 07:22:22 +0900

    Create station model

---
+++ db/migrate/20161223221511_create_stations.rb
@@ -0,0 +1,14 @@
+class CreateStations < ActiveRecord::Migration[5.0]
+  def change
+    create_table :stations do |t|
+      t.integer :line_id, null: false
+      t.string :name, null: false
+
+      t.timestamps
+
+      t.index :line_id
+    end
+
+    add_foreign_key :statings, :lines, column: :line_id, primary_key: :line_id
+  end
+end
---
+++ spec/factories/stations.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :station do
+    line_id 1
+    name "MyString"
+  end
+end
